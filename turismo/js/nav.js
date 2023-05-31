5e21c9f492e517acfefb8e5ea22a91efdb695d39
Igor Rocha <igorestagios@gmail.com>
2023-05-31 16:46:51 -0300

    ideais
    algumas ideias

---
+++ turismo/js/nav.js
@@ -0,0 +1,5 @@
+
+const menuBtn = document.querySelector(".menu-btn");
+const navigation= document.querySelector(".navigation");
+
+menuBtn.addEventListener("click",() =>{navigation.classList.toogle("active");});
